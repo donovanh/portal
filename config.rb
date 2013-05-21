5a83dc62f5c579a6a22709513b087db8021602ea
Donovan Hutchinson <don@converser.io>
2013-05-21 23:54:29 +0100

    More compact css

--- config.rb
+++ config.rb
@@ -9,11 +9,12 @@ javascripts_dir = "javascripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
+output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 preferred_syntax = :sass
